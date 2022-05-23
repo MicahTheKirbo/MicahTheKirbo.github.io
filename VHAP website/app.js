6e20233c6b3541301f5c11b3f05ef6e87a3647e5
MicahTheKirbo <99162104+MicahTheKirbo@users.noreply.github.com>
2022-05-23 20:41:08 +1000

    Update app.js

--- VHAP website/app.js
+++ VHAP website/app.js
@@ -15,10 +15,14 @@ function calculate() {
         myArr = myArr.sort((a, b) => b - a);
         let bigNum = parseInt(myArr.join(''));
         
+        
         myArr = myArr.sort((a, b) => a - b);
         let smallNum = parseInt(myArr.join(''));
         
         number = bigNum - smallNum;
+        if (number == 99) {
+            number = 990;
+        }
         console.log(number);
         string = string + number + " => "
     } while (!(number == 0 || number == 495));
